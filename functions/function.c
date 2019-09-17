030656139092200ff31c3cbd75be8cfa6eddeea7
Joao Luis Moraes <joaoluismoraes@Joaos-MacBook-Air.local>
2019-09-17 19:13:26 -0300

    cube function

--- functions/function.c
+++ functions/function.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// prototyping so that main() is always on top
+double cube(double num);
+
 int main()
 {
-  printf("oiiiiii");
+  printf("Answer: %f", cube(5));
+
+  return 0;
+}
+
+double cube(double num) {
+  return num * num * num;
 }
